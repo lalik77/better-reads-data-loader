58bdde2ed4685fc983e3166ba1dfa221426eb238
Alexander Mami <algru81@gmail.com>
2022-01-10 22:25:57 +0300

    finish video 7 , success

--- src/main/java/com/mami/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/mami/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,13 +1,26 @@
 package com.mami.betterreadsdataloader;
 
+import com.mami.betterreadsdataloader.connection.DataStaxAstraConfig;
+import java.nio.file.Path;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BetterreadsDataLoaderApplication {
+@EnableConfigurationProperties(DataStaxAstraConfig.class)
+public class BetterReadsDataLoaderApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
+    SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+  }
+
+
+  @Bean
+  public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraConfig config) {
+    Path bundle = config.getSecureConnectBundle().toPath();
+    return builder->builder.withCloudSecureConnectBundle(bundle);
   }
 
 }
