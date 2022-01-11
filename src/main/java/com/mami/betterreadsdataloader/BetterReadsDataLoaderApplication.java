74c150d624bdb5fbb8c869936de5df54539bc43d
Alexander Mami <algru81@gmail.com>
2022-01-11 18:50:10 +0300

    finish video 8

--- src/main/java/com/mami/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/mami/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,7 +1,11 @@
 package com.mami.betterreadsdataloader;
 
+import com.mami.betterreadsdataloader.author.Author;
+import com.mami.betterreadsdataloader.author.AuthorRepository;
 import com.mami.betterreadsdataloader.connection.DataStaxAstraConfig;
 import java.nio.file.Path;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -12,15 +16,26 @@ import org.springframework.context.annotation.Bean;
 @EnableConfigurationProperties(DataStaxAstraConfig.class)
 public class BetterReadsDataLoaderApplication {
 
+  @Autowired
+  private AuthorRepository authorRepository;
+
   public static void main(String[] args) {
     SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
   }
 
+  @PostConstruct
+  public void start() {
+    System.out.println("[Start]==============================>       Application Started!  < ==============================");
+    Author author=new Author();
+    author.setId("id");
+    author.setName("name");
+    author.setPersonalName("personalName");
+    authorRepository.save(author);
+  }
 
   @Bean
   public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraConfig config) {
     Path bundle = config.getSecureConnectBundle().toPath();
     return builder->builder.withCloudSecureConnectBundle(bundle);
   }
-
 }
